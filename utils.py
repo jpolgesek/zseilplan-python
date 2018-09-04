e96d1c613968968a8e2effb49e986ab53be632df
Jakub Półgęsek <jakub@polgesek.pl>
2018-09-04 17:40:51 +0200

    Change hashing algorithm (for python < 3.7)

--- utils.py
+++ utils.py
@@ -6,12 +6,12 @@ import json
 def hash_output(output):
 	hash_input = ""
 	hash_input += output["_updateDate_min"] + "," + output["_updateDate_max"]
-	hash_input += json.dumps(output['timetable'])
-	hash_input += ",".join(output['teachers'])
-	hash_input += ",".join(output['units'])
-	hash_input += ",".join(output['classrooms'])
-	hash_input += ",".join(output['teachermap'])
-	hash_input += ",".join(output['timesteps'])
+	hash_input += json.dumps(output['timetable'], sort_keys=True)
+	hash_input += json.dumps(output['teachers'], sort_keys=True)
+	hash_input += json.dumps(output['units'], sort_keys=True)
+	hash_input += json.dumps(output['classrooms'], sort_keys=True)
+	hash_input += json.dumps(output['teachermap'], sort_keys=True)
+	hash_input += json.dumps(output['timesteps'], sort_keys=True)
 	
 	hash_object = hashlib.sha256(hash_input.encode("UTF-8"))
 	hex_dig = hash_object.hexdigest()
