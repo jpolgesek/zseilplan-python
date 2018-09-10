13197117d5b2748483ef756ab044857ede33358c
Administrator <jakub@polgesek.pl>
2018-09-10 22:25:40 +0200

    New notification engine

--- notifications.py
+++ notifications.py
@@ -2,11 +2,17 @@
 import requests
 import json
 from pywebpush import webpush
+import os
+import config
+import datetime
+import ftplib
+from collections import OrderedDict
+
+target = {}
+
+ARCHIVE_DIR = "archive"
 
 PRIVATE_KEY = "***REMOVED***"
-
-r = requests.get("https://testplan.polgesek.pl/subscriptions.json")
-subs = r.json()
 
 def send_push(s, data):
 	aud = s["endpoint"].split("https://")[1].split("/")[0]
@@ -23,6 +29,36 @@ data = {
 	"body": "Już jest! Nowy, lepszy plan. Nie było już przecież zmiany od {} dni".format("__TODO__"),
 	"icon": 'img/launcher-icon-4x.png'
 }
+
+
+with open(os.path.join(ARCHIVE_DIR, "internal_index.json"), "r") as f:
+	hashes = json.load(f)
+
+hashes_2 = {}
+for k in hashes:
+	hashes_2[hashes[k]] = k
+hashes_2 = sorted(hashes_2)
+
+date_new = hashes_2[-1]
+date_old = hashes_2[-2]
+
+date_new = datetime.datetime.strptime(date_new, "%d.%m.%Y %H:%M:%S")
+date_old = datetime.datetime.strptime(date_old, "%d.%m.%Y %H:%M:%S")
+
+difference = date_new - date_old
+difference_hour = difference.seconds//3600
+difference_minute = (difference.seconds//60)%60
+
+if difference.days == 0:
+	if difference_hour == 0:
+		difference_string = "{} minut".format(difference_minute)
+	else:
+		difference_string = "{} godzin".format(difference_hour)
+elif difference.days == 1:
+	difference_string = "{} dnia".format(difference.days)
+else:
+	difference_string = "{} dni".format(difference.days)
+
 
 #data = {}
 
@@ -47,6 +83,8 @@ def send_to(recipient, title="Super Clever Plan", message="", url="https://dev.p
 
 
 def send_to_all(title="Super Clever Plan", message="", url="https://dev.polgesek.pl/zseilplan/", icon="assets/img/launcher-icon-4x.png"):
+	message = "Już jest! Nowy, lepszy plan. Nie było już przecież zmiany od {}".format(difference_string)
+
 	for k in subs:
 		send_to(subs[k], title=title, message=message, url=url, icon=icon)
 	return True
@@ -55,4 +93,33 @@ def send_to_all(title="Super Clever Plan", message="", url="https://dev.polgesek
 if __name__ == "__main__":
 	print("This should not be called directly")
 	# send_to_all(title="Super promocja!", message="Teraz w pizzy hut duża pizza tylko 29,99. A przy okazji testuję cross-origin i nowy silnik powiadomień")
-	send_to_all(title="Super Clever Plan - Nowy Plan", message="Już jest! Nowy, lepszy plan. Nie było już przecież zmiany od {} dni".format("__TODO__"))
+	send_to_all(title="Super Clever Plan - Nowy Plan")
+
+
+
+def start():
+	global subs
+
+	print("Get subscriptions list")
+
+	ftp = ftplib.FTP(target['hostname'])
+	ftp.login(user = target['ftp_user'], passwd = target['ftp_pass'])
+	ftp.cwd(target['ftp_rootdir_app'])
+	
+	with open(".htsubscriptions.json", 'wb') as f:
+		ftp.retrbinary('RETR .htsubscriptions.json', f.write)
+		
+	with open(".htsubscriptions.json", 'r', encoding="UTF-8") as f:
+		subs = json.load(f)
+	
+	print("Ilość subskrybcji: {}".format(len(subs)))
+
+	send_to_all()
+	
+	print("Update subscriptions list [todo]")
+	'''
+	ftp = ftplib.FTP(target['hostname'])
+	ftp.login(user = target['ftp_user'], passwd = target['ftp_pass'])
+	ftp.cwd(target['ftp_rootdir_app'])
+	ftp.storbinary('STOR .htsubscriptions.json', open(".htsubscriptions.json", 'rb'))
+	'''
