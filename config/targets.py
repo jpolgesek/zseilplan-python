bd7453e6b3cc7ac68739e9fd6ec6a548a6d84f68
Jakub Półgęsek <jakub@polgesek.pl>
2019-06-07 20:05:14 +0200

    Add example target configuration

---
+++ config/targets.py
@@ -0,0 +1,14 @@
+targets = {}
+
+targets["example"] = {
+	"dev": True,
+	"http_rootdir_app": "/",
+	"http_rootdir_manifest": "/",
+	"hostname": "localhost",
+	"upload": True,
+	"uploader": "local",
+	"local": {
+		"rootdir_manifest": ".",
+		"rootdir_app": "."
+	}
+}
