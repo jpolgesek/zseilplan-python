e5727a1068d5dab0a6c48c5601bd85f72a4b339f
Jakub Półgęsek <jakub@polgesek.pl>
2020-09-29 21:57:12 +0200

    WIP: Teacher timetable parsing

--- app/zseilplan/modules/TimetableWWWProvider.py
+++ app/zseilplan/modules/TimetableWWWProvider.py
@@ -24,6 +24,7 @@ class TimetableData:
         # self.new_teachers_timetable = OrderedDict()
         self.teacher_recovery = {}
         self.auto_timesteps = []
+        self.best_timesteps = []
 
 class RawTimetableItem:
     def __init__(self, unit: str, day: int, hour: int, html_item):
@@ -70,20 +71,21 @@ class RawTimetableItem:
     
 
     def get_teacher(self, parser_object):
-        return "Testowy Test"
+        # return "Testowy Test"
         #workaroud for timetables, where are duplicate entries for subject, but there are none for teacher
         if len(self.find_by_class(parser_object.getElementsByTagName("span"),"p")) > 1:
             # Try to find teacher_recovery_mapping
             pseudo_teacher = unidecode(self.find_by_class(parser_object.getElementsByTagName("span"),"p")[1].innerText.upper())
-            if pseudo_teacher in self.teacher_recovery:
-                return self.teacher_recovery[pseudo_teacher]
-            else:
-                print("Nie znalazlem recovery dla {}".format(pseudo_teacher))
-                return pseudo_teacher
+            return pseudo_teacher
+            # if pseudo_teacher in self.teacher_recovery:
+            #     return self.teacher_recovery[pseudo_teacher]
+            # else:
+            #     print("Nie znalazlem recovery dla {}".format(pseudo_teacher))
+            #     return pseudo_teacher
         else:
-            pseudo_teacher = unidecode(self.find_by_class(parser_object.getElementsByTagName("span"),"n")[0].innerText.upper())
-            if pseudo_teacher in self.teacher_recovery:
-                return self.teacher_recovery[pseudo_teacher]
+            # pseudo_teacher = unidecode(self.find_by_class(parser_object.getElementsByTagName("span"),"n")[0].innerText.upper())
+            # if pseudo_teacher in self.teacher_recovery:
+            #     return self.teacher_recovery[pseudo_teacher]
             return unidecode(self.find_by_class(parser_object.getElementsByTagName("span"),"n")[0].innerText.upper())
     
     def parse(self):
@@ -168,7 +170,7 @@ class TimetableWWWProvider:
 
             timesteps = []
             for time_block in time_blocks:
-                timesteps += time_block.innerText.split("-")
+                timesteps += list(map(str.strip, time_block.innerText.split("-")))
             
             all_timesteps.append(timesteps)
         
@@ -176,6 +178,8 @@ class TimetableWWWProvider:
         
     def import_teachers(self):
         print("TODO: Import teachers from full vulcan www")
+        for key in self.timetable_data.teacher_timetable.keys():
+            self.timetable_data.teachers[key] = f"{key} [NOT IMPLEMENTED]"
         return True
     
     def add_item_to_teacher_timetable(self, item: TimetableItem):
@@ -189,13 +193,9 @@ class TimetableWWWProvider:
         
         # If this is the first iteration of this lesson in this day, create dict
         if item.hour not in self.timetable_data.teacher_timetable[item.teacher][item.day]:
-            self.timetable_data.teacher_timetable[item.teacher][item.day][item.hour] = {}
+            self.timetable_data.teacher_timetable[item.teacher][item.day][item.hour] = []
         
-        # If this is the first iteration of this unit lesson in this day, create array
-        if item.unit not in self.timetable_data.teacher_timetable[item.teacher][item.day][item.hour]:
-            self.timetable_data.teacher_timetable[item.teacher][item.day][item.hour][item.unit] = []
-        
-        self.timetable_data.teacher_timetable[item.teacher][item.day][item.hour][item.unit].append(item.serialize())
+        self.timetable_data.teacher_timetable[item.teacher][item.day][item.hour].append(item.serialize())
         return True
     
     def add_item_to_timetable(self, item: TimetableItem):
@@ -233,6 +233,29 @@ class TimetableWWWProvider:
         
         return True
 
+    def add_raw_item(self, day: int, hour: int, unit: str, raw_item):
+        item = RawTimetableItem(unit = unit, 
+                                day = day, 
+                                hour = hour, 
+                                html_item = raw_item)
+        return self.add_item_to_timetable(item.parse())
+
+    def parse_single_cell(self, day: int, hour: int, unit: str, cell):
+        if cell.getElementsByTagName("span")[0].className == "p":
+            zseilplan.modules.utils.debug("Found container with single subject")
+            self.add_raw_item(day = day, hour = hour, unit = unit, raw_item = cell)
+            return
+
+        zseilplan.modules.utils.debug("Container missing, manual search")
+
+        for parent in cell.getElementsByTagName("span"):
+            parent = parent.getChildren()
+
+            if len(parent) == 0:
+                continue
+
+            self.add_raw_item(day = day, hour = hour, unit = unit, raw_item = parent)
+
     def import_timetable_for_unit(self, unit_name, unit_url):
         resp = self.http_get(unit_url)
         if resp.status_code != 200:
@@ -240,91 +263,43 @@ class TimetableWWWProvider:
         
         self.parser.parseStr(resp.text)
         
-        # Get update date of *this* unit timetable
+        # Get update date of this timetable
         update_date = self.parser.getElementsByAttr("align","right")[0][0][0][0].innerText.split('\r\n')[1].split(" ")[1]
         self.timetable_data.update_dates.append(update_date)
         
         rows = self.parser.getAllNodes().getElementsByClassName("tabela")[0].getChildren()
 
         for hour,row in enumerate(rows):
-
-            # MAYBE ITS FUCKING TIME TO DECIDE?
-            # (and change to count from 0)
-            day = 0 #count from 0, because WTF
-            #day = 1 #count from 1, because backwards compatibility
-
             columns = row.getElementsByClassName("l")
-            for column in columns:
+
+            for day, column in enumerate(columns):
+                #FIXME: Counting from 1...
                 day += 1
                 
                 #Empty - skip
                 if column.innerText == "&nbsp;":
                     continue
 
-                zseilplan.modules.utils.debug("Dzien {} - {} godzina lekcyjna".format(day, hour), level=2)
-                
-                '''TODO: make this a func'''
-
-                # If this is the first iteration of this day, create dict
-                if day not in self.timetable_data.timetable:
-                    self.timetable_data.timetable[day] = {}
-                
-                # If this is the first iteration of this lesson in this day, create dict
-                if hour not in self.timetable_data.timetable[day]:
-                    self.timetable_data.timetable[day][hour] = {}
-            
-                # If this is the first iteration of this unit lesson in this day, create array
-                if unit_name not in self.timetable_data.timetable[day][hour]:
-                    self.timetable_data.timetable[day][hour][unit_name] = []
-
-                '''END TODO'''
-
-
+                zseilplan.modules.utils.debug(f"Day: {day} | Hour: {hour}", level=2)
                 entries = column.innerHTML.split("<br />")
 
-                for e in entries:
-                    entry = AdvancedHTMLParser()
-                    entry.parseStr(e)
-                    
-                    if entry.getElementsByTagName("span")[0].className == "p":
-                        zseilplan.modules.utils.debug("Found container with single subject")
-
-                        item = RawTimetableItem(unit = unit_name, 
-                                                day = day, 
-                                                hour = hour, 
-                                                html_item = entry)
-                        self.add_item_to_timetable(item.parse())
-                        
-                        # self.add_to_teacher_plan(subject["p"], subject["n"], subject["s"], unit_name, day, hour)
-                        # try:
-                        #     self.add_to_new_teacher_plan(subject["p"], subject["n"], subject["s"], unit_name, day, hour)
-                        # except:
-                        #     pass
-                    else:
-                        zseilplan.modules.utils.debug("Container missing, manual search")
-
-                        for parent in entry.getElementsByTagName("span"):
-                            parent = parent.getChildren()
-                            
-                            if len(parent) == 0:
-                                continue
-
-
-                            item = RawTimetableItem(unit = unit_name, 
-                                                    day = day, 
-                                                    hour = hour, 
-                                                    html_item = parent)
-                            
-                            self.add_item_to_timetable(item.parse())
-
-                            # self.add_to_teacher_plan(subject["p"], subject["n"], subject["s"], unit_name, day, hour)
-                            # try:
-                            #     self.add_to_new_teacher_plan(subject["p"], subject["n"], subject["s"], unit_name, day, hour)
-                            # except:
-                            #     pass
+                for raw_entry in entries:
+                    cell = AdvancedHTMLParser()
+                    cell.parseStr(raw_entry)
+                    self.parse_single_cell(day = day, hour = hour, unit = unit_name, cell = cell)
         
         return True
+
+    def find_best_timesteps(self):
+        current_record = []
+
+        for timestep in self.timetable_data.auto_timesteps[0]:
+            if len(timestep) > len(current_record):
+                current_record = timestep
         
+        self.timetable_data.best_timesteps = current_record
+        return
+    
     def do_things(self):
         print("Finding units")
         self._units = self.get_units_urls()
@@ -333,12 +308,14 @@ class TimetableWWWProvider:
         print("Searching for optimal timesteps")
         z = self.import_timesteps()
         self.timetable_data.auto_timesteps.append(z)
+        self.find_best_timesteps()
 
-        print("Importing teachers")
-        z = self.import_teachers()
 
 
         self.import_timetable()
+
+        print("Importing teachers")
+        z = self.import_teachers()
 
 
         with open("test.json", "w", encoding = "UTF-8") as f:
@@ -351,11 +328,11 @@ class TimetableWWWProvider:
             output["_updateDate_min"] = min(self.timetable_data.update_dates)
             output["_updateDate_max"] = max(self.timetable_data.update_dates)
 
-            output['timesteps'] = {"default": self.timetable_data.auto_timesteps[0][0]}
+            output['timesteps'] = {"default": self.timetable_data.best_timesteps}
             output['teachers'] = self.timetable_data.teachers
             output['timetable'] = self.timetable_data.timetable
             output['units'] = sorted(self.timetable_data.units)
-            output["teachermap"] = {"Testowy Test": "Testowy Test Test"}
+            output["teachermap"] = self.timetable_data.teachers
             output['classrooms'] = sorted(self.timetable_data.classrooms)
 
             json.dump(output, f)
